42c00147ce3d90df7aba89e2f93b360fb5c68073
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-03-19 16:42:03 -0300

    att

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,9 @@
+function tocaSom(idAudio) {
+    document.querySelector(idAudio).play();
+}
+
 const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    listaDeTeclas[contador].onclick = tocaSom;    
+}
